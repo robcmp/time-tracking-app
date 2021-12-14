3f77f7695588f074c7d6f948d81ad63f199873a9
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-12-14 08:22:26 -0300

    added EditableTimer component

--- public/js/app.js
+++ public/js/app.js
@@ -33,3 +33,22 @@ class EditableTimerList extends React.Component {
     );
   }
 }
+
+class EditableTimer extends React.Component {
+  render() {
+    if (this.props.editFormOpen) {
+      return (
+        <TimerForm title={this.props.title} project={this.props.project} />
+      );
+    } else {
+      return (
+        <Timer
+          title={this.props.title}
+          project={this.props.project}
+          elapsed={this.props.elapsed}
+          runningSince={this.props.runningSince}
+        />
+      );
+    }
+  }
+}
