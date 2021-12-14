c4e13793a591ab54d6d48f2386cc3a9c47fa0df7
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-12-14 08:36:54 -0300

    added TimerForm component

--- public/js/app.js
+++ public/js/app.js
@@ -52,3 +52,28 @@ class EditableTimer extends React.Component {
     }
   }
 }
+
+class TimerForm extends React.Component {
+  render() {
+    return (
+      <div className="ui centered card">
+        <div className="content">
+          <div className="ui form">
+            <div className="field">
+              <label>Title</label>
+              <input type="text" defaultValue={this.props.title} />
+            </div>
+            <div className="field">
+              <label>Project</label>
+              <input type="text" defaultValue={this.props.project} />
+            </div>
+            <div className="ui two bottom attached buttons">
+              <button className="ui basic blue button">{submitText}</button>
+              <button className="ui basic red button">Cancel</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
