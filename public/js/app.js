2651259c31320ea014c9e9021db974227327022e
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-12-14 08:06:03 -0300

    created TimerDashboard and EditableTimerList components

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,35 @@
+class TimerDashboard extends React.Component {
+  render() {
+    return (
+      <div className="ui three column centered grid">
+        <div className="column">
+          <EditableTimerList />
+          <ToggleableTimerForm isOpen={true} />
+        </div>
+      </div>
+    );
+  }
+}
 
+class EditableTimerList extends React.Component {
+  render() {
+    return (
+      <div id="timers">
+        <EditableTimer
+          title="Learn React"
+          project="Web Domination"
+          elapsed="545648"
+          runningSince={null}
+          editFormOpen={false}
+        />
+        <EditableTimer
+          title="Learn extreme ironing"
+          project="World Domination"
+          elapsed="7877211"
+          runningSince={null}
+          editFormOpen={true}
+        />
+      </div>
+    );
+  }
+}
