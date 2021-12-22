8048706827792773499d18c0dd58252c4934023e
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-12-22 08:38:14 -0300

    added hardcoded state to TimersDashboards

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,22 @@
 class TimersDashboard extends React.Component {
+  state = {
+    timers: [
+      {
+        title: "Practice squat",
+        project: "Gym Chores",
+        id: uuid.v4(),
+        elapsed: 5456099,
+        runningSince: Date.now(),
+      },
+      {
+        title: "Bake squash",
+        project: "Kitchen Chores",
+        id: uuid.v4(),
+        elapsed: 1273998,
+        runningSince: null,
+      },
+    ],
+  };
   render() {
     return (
       <div className="ui three column centered grid">
